0d5e84a0e569a04281e1353b3e91cc414cb1f0d9
Fabryzzio-Meza-Torres <fabryzziomt@gmail.com>
2024-09-24 01:47:15 -0500

    Advance LL1 parser

--- LL1_parser.cpp
+++ LL1_parser.cpp
@@ -1,82 +1,120 @@
 #include <iostream>
-#include <stack>
-#include <unordered_map>
-#include <vector>
 #include <string>
+#include <vector>
+#include <stack>
+#include <map>  
+#include <iomanip>  
 
 using namespace std;
 
-// Definición de tokens y scanner
-class Token {
+class Token{
 public:
-    Token()=default;
+    Token() = default;
     enum Type { ID, SEMICOLON, ASSIGN, PRINT, LPAREN, RPAREN, PLUS, MINUS, MULT, NUM, END, ERR };
     static const char* token_names[12];
     Type type;
     string lexema;
-    Token(Type type):type(type) { }
-    Token(Type type, const string& lex):type(type), lexema(lex) { }
+    explicit Token(Type type) : type(type) { }
+    explicit Token(Type type, const string& lex) : type(type), lexema(lex) {}
 };
 
-const char* Token::token_names[12] = {"id", ";", "=", "print", "(", ")", "+", "-", "*", "Num", "$", "ERR"};
+const char* Token::token_names[12] = { "id", ";", "=", "print", "(", ")", "+", "-", "*", "Num", "$", "Err" };
 
-ostream& operator << (ostream& os, const Token& tok) {
-    return os << Token::token_names[tok.type] << " (" << tok.lexema << ")";
+ostream& operator<<(ostream& os, Token tokencito) {
+    return os << Token::token_names[tokencito.type] << "(" << tokencito.lexema << ")";
 }
 
-// Simulando un escáner que tokeniza la entrada
-class Scanner {
+struct Scanner {
+private:
+    string input;
+    size_t pos;
+    
 public:
-    Scanner(const string& in) : input(in), pos(0) {}
-    Token nextToken() {
+    Scanner(const string& uwu) : input(uwu), pos(0) {}
+    Token next_Token() {
         while (pos < input.size() && isspace(input[pos])) pos++;
-        if (pos >= input.size()) return Token(Token::END);
+        if (pos >= input.size()) {
+            return Token(Token::END);
+        }
 
         char current = input[pos];
         if (isalpha(current)) {
             string id;
-            while (pos < input.size() && (isalnum(input[pos]) || input[pos] == '_')) id += input[pos++];
-            if (id == "print") return Token(Token::PRINT, id);
+            while (pos < input.size() && (isalnum(input[pos]) || input[pos] == '_')) {
+                id += input[pos++];
+            }
+            if (id == "print") {
+                return Token(Token::PRINT, id);
+            }
             return Token(Token::ID, id);
         }
+
         if (isdigit(current)) {
             string num;
-            while (pos < input.size() && isdigit(input[pos])) num += input[pos++];
+            while (pos < input.size() && isdigit(input[pos])) {
+                num += input[pos++];
+            }
             return Token(Token::NUM, num);
         }
 
         switch (current) {
-            case ';': pos++; return Token(Token::SEMICOLON, ";");
-            case '=': pos++; return Token(Token::ASSIGN, "=");
-            case '(': pos++; return Token(Token::LPAREN, "(");
-            case ')': pos++; return Token(Token::RPAREN, ")");
-            case '+': pos++; return Token(Token::PLUS, "+");
-            case '-': pos++; return Token(Token::MINUS, "-");
-            case '*': pos++; return Token(Token::MULT, "*");
-            default: return Token(Token::ERR, string(1, current));
+        case ';': pos++; return Token(Token::SEMICOLON, ";");
+        case '=': pos++; return Token(Token::ASSIGN, "=");
+        case '(': pos++; return Token(Token::LPAREN, "(");
+        case ')': pos++; return Token(Token::RPAREN, ")");
+        case '+': pos++; return Token(Token::PLUS, "+");
+        case '-': pos++; return Token(Token::MINUS, "-");
+        case '*': pos++; return Token(Token::MULT, "*");
+        default: return Token(Token::ERR, string(1, current));
         }
     }
-private:
-    string input;
-    size_t pos;
 };
 
-// Parser LL1 con tabla predictiva
-class ParserLL1 {
-public:
-    ParserLL1(const vector<Token>& tokens) : entrada(tokens), index(0) {
-        inicializarTablaLL1();
-        pila.push("$");
-        pila.push("P");
+struct ParserLL1 {
+private:
+    stack<string> stack;
+    vector<Token> input;
+    int index;
+    map<string, map<string, vector<string>>> LL1_Parsing_Table;
+
+    bool isterminal(const string& symbol) {
+        for (const char* terminal : Token::token_names) {
+            if (symbol == terminal) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    void inicializarTablaLL1() {
-        // LL1 Parsing Table
+    void print_state(const string& action) {
+        std::stack<string> stacktemp = this->stack;
+        string stackstr;
+        while (!stacktemp.empty()) {
+            stackstr = stacktemp.top() + " " + stackstr;
+            stacktemp.pop();
+        }
+
+        string inputstr;
+        for (size_t i = index; i < input.size(); ++i) {
+            inputstr += input[i].lexema + " ";
+        }
+
+        cout << setw(30)<< stackstr << setw(30) << inputstr << setw(30) << action << "\n";
+    }
+
+public:
+    ParserLL1(const vector<Token>& tokens) : input(tokens), index(0) {
+        initialize_LL1_Parsing_Table();
+        stack.push("$");
+        stack.push("P");
+    }
+
+    void initialize_LL1_Parsing_Table() {
         LL1_Parsing_Table = {
             {"P", {{"id", {"SL"}}, {"print", {"SL"}}}},
             {"SL", {{"id", {"S", "SL'"}}, {"print", {"S", "SL'"}}}},
-            {"SL'", {{";", {"S", "SL'"}}, {"$", {""}}}},
-            {"S", {{"id", {"id", "=", "E"}}, {"print", {"print", "E"}}}},
+            {"SL'", {{";", {"S", "SL'"}}, {"$", {""}}}},  
+            {"S", {{"id", {"id", "=", "E", ";"}}, {"print", {"print", "E", ";"}}}},  
             {"E", {{"id", {"T", "E'"}}, {"Num", {"T", "E'"}}, {"(", {"T", "E'"}}}},
             {"E'", {{";", {""}}, {")", {""}}, {"+", {"+", "T", "E'"}}, {"-", {"-", "T", "E'"}}, {"$", {""}}}},
             {"T", {{"id", {"F", "T'"}}, {"Num", {"F", "T'"}}, {"(", {"F", "T'"}}}},
@@ -85,96 +123,58 @@ public:
         };
     }
 
-    void parsear() {
-        cout << "Pila \t\t Entrada \t\t Acción" << endl;
-        imprimirEstado(); // Imprimir el estado inicial
+    void parse() {
+        cout << setw(30) << "Pila" << setw(30) << "Entrada" << setw(30) << "Acción" << endl;
 
-        while (!pila.empty()) {
-            string top = pila.top();
-            pila.pop();
+        while (!stack.empty()) {
+            string top = stack.top();
 
-            Token tokenActual = entrada[index];
+            Token tokenActual = input[index];
 
-            if (esTerminal(top)) {
+            if (isterminal(top)) {
                 if (top == tokenActual.lexema || top == Token::token_names[tokenActual.type]) {
-                    cout << "Match: " << top << endl;
+                    print_state("match " + top);
                     index++;
+                    stack.pop();
                 } else {
                     cout << "Error de sintaxis: se esperaba " << top << endl;
                     return;
                 }
             } else {
                 string entradaActual = Token::token_names[tokenActual.type];
-                if (LL1_Parsing_Table[top][entradaActual].size() > 0) {
+                if (LL1_Parsing_Table[top].find(entradaActual) != LL1_Parsing_Table[top].end()) {
                     vector<string> produccion = LL1_Parsing_Table[top][entradaActual];
-                    cout << top << " -> ";
-                    for (auto &p : produccion) cout << p << " ";
-                    cout << endl;
+                    string action = top + " -> ";
+                    for (const auto& p : produccion) action += p + " ";
+                    print_state(action);
+                    stack.pop();
 
-                    // Apilar producción en orden inverso
                     for (auto it = produccion.rbegin(); it != produccion.rend(); ++it) {
-                        if (*it != "") pila.push(*it);
+                        if (*it != "") stack.push(*it);
                     }
                 } else {
-                    cout << "Error de sintaxis: no se encontró entrada para " << entradaActual << endl;
+                    cout << "Error de sintaxis: no se encontró entrada para " << tokenActual.lexema << endl;
                     return;
                 }
             }
-
-            imprimirEstado(); // Imprimir el estado después de cada acción
-        }
-    }
-
-private:
-    stack<string> pila;
-    vector<Token> entrada;
-    int index;
-
-    unordered_map<string, unordered_map<string, vector<string>>> LL1_Parsing_Table;
-
-    bool esTerminal(const string& simbolo) {
-        for (const char* terminal : Token::token_names)
-            if (simbolo == terminal) return true;
-        return false;
-    }
-
-    void imprimirEstado() {
-        // Imprimir el contenido de la pila
-        stack<string> pilaTmp = pila;
-        string pilaStr;
-        while (!pilaTmp.empty()) {
-            pilaStr = pilaTmp.top() + " " + pilaStr;
-            pilaTmp.pop();
         }
 
-        // Imprimir la entrada restante
-        string entradaStr;
-        for (size_t i = index; i < entrada.size(); ++i) {
-            entradaStr += entrada[i].lexema + " ";
-        }
-
-        // Imprimir estado actual
-        cout << pilaStr << "\t\t" << entradaStr << "\t\tAcción realizada" << endl;
+        cout << "Cadena aceptada!" << endl;
     }
 };
 
 int main() {
-    string input = "x = 4 + 3; print(x + 1);";
-    Scanner scanner(input);
+    string codigo = "x = 4";
+    Scanner scanner(codigo);
     vector<Token> tokens;
-    Token token;
-    while ((token = scanner.nextToken()).type != Token::END) {
-        tokens.push_back(token);
+    Token tokencito;
+    while ((tokencito = scanner.next_Token()).type != Token::END) {
+        tokens.push_back(tokencito);
     }
-    tokens.push_back(Token(Token::END, "$"));  // Añadir token de fin
+    tokens.push_back(Token(Token::END, "$"));
 
     ParserLL1 parser(tokens);
-    parser.parsear();
+    parser.parse();
 
     return 0;
 }
-
-
-
-
-
